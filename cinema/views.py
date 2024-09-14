3e8f5cb59bdf93294ec2db83181f01f98c02a7a8
reichikku <bladeryz@gmail.com>
2024-09-14 13:46:15 +0300

    Fixes

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,3 @@
-from wsgiref.util import request_uri
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
